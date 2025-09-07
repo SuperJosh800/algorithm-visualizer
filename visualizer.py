f35f14b0ce8c4a74bb2a718e0edcb1c9bcaf7d44
Joshua Conetta <147725463+SuperJosh800@users.noreply.github.com>
2025-09-07 16:45:42 +0100

    Draw in informtaion class added

--- visualizer.py
+++ visualizer.py
@@ -1,1 +1,33 @@
-print("Git test")
+import pygame
+import random
+pygame.init() 
+
+class DrawInformation:
+    BLACK = 0, 0, 0
+    WHITE = 255, 255, 255
+    GREEN = 0, 255, 0
+    RED = 255, 0, 0
+    GREY = 128, 128, 128
+    BACKGROUND_COLOUR = WHITE
+
+    TOP_PAD = 150
+    SIDE_PAD = 100 
+
+    def __init__(self, width, height, lst):
+        self.width = width
+        self.height = height
+
+        self.window = pygame.display.set_mode((width, height))
+        pygame.display.set_caption("Sorting Algorithm Visual")
+        self.set_list(lst)
+
+    def set_list(self, lst):
+        self.lst = lst
+        self.min_value = min(lst)
+        self.max_value = max(lst)
+
+        self.block_width = round((self.width - self.SIDE_PAD) / len(lst))
+        self.block_height = round((self.height - self.TOP_PAD) / (self.max_value - self.min_value))
+        self.start_x = self.SIDE_PAD // 2
+
+
