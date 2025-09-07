0b5ab0213e6e78342165b1452f03e766429be5fc
Joshua Conetta <147725463+SuperJosh800@users.noreply.github.com>
2025-09-07 17:13:03 +0100

    Added drawing of rectangles using pygame.
    uses randomiser to pick x and y and then calulates where to start drawing based on the padding

--- visualizer.py
+++ visualizer.py
@@ -7,8 +7,13 @@ class DrawInformation:
     WHITE = 255, 255, 255
     GREEN = 0, 255, 0
     RED = 255, 0, 0
-    GREY = 128, 128, 128
     BACKGROUND_COLOUR = WHITE
+
+    GRAYS = [
+        (128, 128, 128),
+        (165, 165, 165),
+        (197, 197, 197)
+    ]
 
     TOP_PAD = 150
     SIDE_PAD = 100 
@@ -30,4 +35,56 @@ class DrawInformation:
         self.block_height = round((self.height - self.TOP_PAD) / (self.max_value - self.min_value))
         self.start_x = self.SIDE_PAD // 2
 
+def draw(draw_info):
+    draw_info.window.fill(draw_info.BACKGROUND_COLOUR)
+    draw_list(draw_info)
+    pygame.display.update()
 
+
+def draw_list(draw_info):
+    lst = draw_info.lst
+
+    for i, val in enumerate(lst):
+        x = draw_info.start_x + i * draw_info.block_width
+        y = draw_info.height - (val - draw_info.min_value) * draw_info.block_height
+
+        colour = draw_info.GRAYS[i % 3] 
+
+        pygame.draw.rect(draw_info.window, colour, (x, y, draw_info.block_width, draw_info.height))
+
+
+def generate_starting_list(n, min_value, max_value):
+    lst = []
+
+    for _ in range(n):
+        val = random.randint(min_value, max_value)
+        lst.append(val)
+    
+    return lst
+
+
+def main():
+    run = True
+    clock = pygame.time.Clock()
+
+    n = 50
+    min_value = 0
+    max_value = 100
+
+    lst = generate_starting_list(n, min_value, max_value)
+    draw_info = DrawInformation(800, 600, lst)
+
+    while run:
+        clock.tick(60)
+
+        draw(draw_info)
+
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT: #lets us quit the game by clicking the x
+                run = False
+
+    pygame.quit()
+
+
+if __name__ == "__main__":
+    main()
